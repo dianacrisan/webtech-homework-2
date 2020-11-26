acfbf7b78726374e30fdae7c31f2ee71e4e8298c
Diana Crisan <crisanmonica18@stud.ase.ro>
2020-11-26 11:24:41 +0200

    finished first 3 tasks

--- main/app.js
+++ main/app.js
@@ -1,5 +1,26 @@
 function addTokens(input, tokens){
-    
+    if (typeof input != 'string' ) {
+        throw new Error('Invalid input');
+    }
+    else if(input.length < 6) {
+        throw new Error('Input should have at least 6 characters');   
+    }
+
+    if(typeof tokens != '{tokenName:string}') {
+        throw new Error('Invalid array format');
+    }
+
+    //If `input` don't contain any `...` return the initial value of `input`
+    //If `input` contains `...`, replace them with the specific values and return the new `input`
+    if(input.contains('...')) {
+        tokens.forEach(el => {
+            input = input.replace('...', '${' + `${el.tokenName}` + '}');
+        });
+        return input;
+    }
+    else {
+        return input;
+    }
 }
 
 const app = {
